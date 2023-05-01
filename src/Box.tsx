cc70ee7ed66c98bff7862b09932ce77019db570e
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-05-01 16:33:49 -0700

    initial commit w typing on Box

---
+++ src/Box.tsx
@@ -0,0 +1,47 @@
+/** Colored box presentation
+ *
+ * Props:
+ * - id (unique)
+ * - width
+ * - height
+ * - backgroundColor
+ * - remove (function to call)
+ *
+ * BoxList -> Box
+ */
+
+
+interface IBox {
+  id: string,
+  width: number,
+  height: number,
+  backgroundColor: string,
+  remove: (arg0: string) => void,
+}
+
+function Box({ id, width = 5, height = 5, backgroundColor, remove }: IBox): JSX.Element {
+
+  /** Remove a box. */
+  function handleRemove():void {
+    remove(id);
+  }
+
+  return (
+    <div className="Box">
+      <div className="Box-box"
+        style={{
+          height: `${height}em`,
+          width: `${width}em`,
+          backgroundColor: backgroundColor
+        }}
+      />
+      <button
+        className="Box-removeBtn"
+        onClick={handleRemove}>
+        Remove The Box!
+      </button>
+    </div>
+  );
+}
+
+export default Box;
